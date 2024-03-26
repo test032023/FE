b8f8ea18ff188ab4f9929c3db2dc7e936342541c
Nguyễn Hữu Sang <nguyenhuusang@Host.local>
2024-03-26 18:03:26 +0700

    up env deploy

--- src/config.js
+++ src/config.js
@@ -2,9 +2,9 @@ const CONFIG = {
   NODE_ENV: process.env.NODE_ENV,
   //FACEBOOK_APP_ID: process.env.REACT_APP_FB_APP_ID || 1358744248403791,
   FACEBOOK_PAGE_ID: process.env.REACT_APP_FB_PAGE_ID || 177244112146374,
-  API_URL: process.env.API_URL || 'http://localhost:5000/api/',
-  HOST: process.env.API_URL || 'http://localhost:5000/',
-  SOCKET_URL: process.env.SOCKET_URL || 'http://localhost:5000/api/',
+  API_URL: process.env.API_URL || 'https://jewelrye-store-api.onrender.com/api',
+  HOST: process.env.API_URL || 'https://jewelrye-store-api.onrender.com',
+  SOCKET_URL: process.env.SOCKET_URL || 'https://jewelrye-store-api.onrender.com/api',
 };
 
 export default CONFIG;
